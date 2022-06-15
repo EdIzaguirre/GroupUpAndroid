fa371d7c5dce8f075da45228d0b60f31ecde51cc
EdIzaguirre <55713891+EdIzaguirre@users.noreply.github.com>
2022-06-14 18:16:07 -0700

    close to getting switch

--- app/src/main/java/com/example/groupupandroid/MainActivity.kt
+++ app/src/main/java/com/example/groupupandroid/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.groupupandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.groupupandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.registerButton.setOnClickListener {
+            binding.registerFields.visibility = View.VISIBLE
+            binding.loginFields.visibility = View.GONE
+        }
+
+        binding.loginButton.setOnClickListener {
+            binding.registerFields.visibility = View.GONE
+            binding.loginFields.visibility = View.VISIBLE
+        }
+
+
     }
 }
