9db7ad5deec9f80deb53e05caa0743b6c8cd538c
EdIzaguirre <55713891+EdIzaguirre@users.noreply.github.com>
2022-07-13 13:47:16 -0700

    Added bottomNavMenu

--- app/src/main/java/com/example/groupupandroid/LoginRegisterFragment.kt
+++ app/src/main/java/com/example/groupupandroid/LoginRegisterFragment.kt
@@ -11,16 +11,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.groupupandroid.databinding.ActivityMainBinding
 import com.example.groupupandroid.databinding.FragmentLoginRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-///**
-// * A simple [Fragment] subclass.
-// * Use the [LoginRegisterFragment.newInstance] factory method to
-// * create an instance of this fragment.
-// */
 class LoginRegisterFragment : Fragment(){
     // Getting xml objects
     private var binding: FragmentLoginRegisterBinding? = null
